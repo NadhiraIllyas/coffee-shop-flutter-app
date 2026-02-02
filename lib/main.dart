03a554bd7918986bfdbf760b867eb9d45c78df15
Naadhira Illyas <fathimanadhira2001@gmail.com>
2026-02-02 09:09:41 +0530

    SEU/IS/20/ICT/019 - Updated Main Page

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const LoginScreen(), // start app with login
+      home: const LoginScreen(),
     );
   }
 }
