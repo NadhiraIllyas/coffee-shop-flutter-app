37e35b0e97efe1b75c50ef3250113c762467c704
Naadhira Illyas <fathimanadhira2001@gmail.com>
2026-02-02 09:06:17 +0530

    SEU/IS/20/ICT/019 - Updated Main Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import 'screens/signup.dart';
-=======
 import 'screens/login_screen.dart';
->>>>>>> 17c3deccfbdb4351c983002be4fbaad0712da044
-=======
 import 'screens/login_screen.dart';
 import 'screens/signup.dart';
->>>>>>> 93132947e00c582942449f4d8092607c05e48bf9
+
 
 void main() {
   runApp(const MyApp());
