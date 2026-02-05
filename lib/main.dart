b939f72ebbfcac97862831f6093d3618db109192
Naadhira Illyas <fathimanadhira2001@gmail.com>
2026-02-05 07:45:36 +0530

    SEU/IS/20/ICT/019 - Crfeated Profile Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,10 @@
 import 'package:flutter/material.dart';
-
-import 'screens/signup.dart';
 import 'screens/login_screen.dart';
-
-import 'screens/login_screen.dart';
-
 import 'screens/signup.dart';
-
-
-import 'screens/addtocartpage.dart';
-
-import 'screens/addtocartpage.dart';
-
-
+import 'screens/home_screen.dart';
 import 'screens/cart_screen.dart';
-
-
+import 'screens/profile_screen.dart';
+import 'screens/addtocartpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,7 +17,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Coffee Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          centerTitle: true,
+        ),
+      ),
       home: const LoginScreen(),
+      // Optional: Define routes for cleaner navigation
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignUpScreen(),
+        '/home': (context) => const HomeScreen(),
+        '/cart': (context) => const CartScreen(),
+        '/profile': (context) => const ProfileScreen(),
+      },
     );
   }
 }
